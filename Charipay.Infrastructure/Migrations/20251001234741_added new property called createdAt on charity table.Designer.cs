db8361c7b384637455db8de451d38834ac565e98
nazmulhyder <nhyder.cse131.uiu@gmail.com>
2025-10-02 00:51:50 +0100

    charity controller added;

--- Charipay.Infrastructure/Migrations/20251001234741_added new property called createdAt on charity table.Designer.cs
+++ Charipay.Infrastructure/Migrations/20251001234741_added new property called createdAt on charity table.Designer.cs
@@ -4,6 +4,7 @@ using Charipay.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Charipay.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251001234741_added new property called createdAt on charity table")]
+    partial class addednewpropertycalledcreatedAtoncharitytable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Charipay/Controllers/CharityController.cs
@@ -0,0 +1,34 @@
+﻿using Charipay.Application.Commands.Charities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Charipay.API.Controllers
+{
+    [ApiController] // ✅ Tells ASP.NET Core this is an API controller
+    [Route("api/[controller]")] // ✅ Base route will be api/users
+    public class CharityController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        private readonly ILogger<UsersController> _logger;
+
+        public CharityController(IMediator mediator, ILogger<UsersController> logger)
+        {
+            _mediator = mediator;
+            _logger = logger;
+
+        }
+
+        [HttpPost("CreateChairy")]
+        public async Task<IActionResult> CreateChairy([FromBody] CreateCharityCommand request, CancellationToken token)
+        {
+            var result = await _mediator.Send(request);
+
+            if (result.Message.Contains("exists"))
+                return BadRequest(result);
+
+            return Ok(result);
+            
+
+        }
+    }
+}
